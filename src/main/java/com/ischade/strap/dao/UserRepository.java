722f836f6ece02be7fe9989381234e77a5d6ad68
IshadeMe <mehanizip@mail.ru>
2023-02-20 15:34:24 +0600

    #2

--- src/main/java/com/ischade/strap/dao/UserRepository.java
+++ src/main/java/com/ischade/strap/dao/UserRepository.java
@@ -2,9 +2,11 @@ package com.ischade.strap.dao;
 
 import com.ischade.strap.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    @Query("select u from User u join fetch u.roles where u.login=:login")
     User findUserByLogin(String login);
 }
--- src/main/java/com/ischade/strap/service/UserServiceImpl.java
+++ src/main/java/com/ischade/strap/service/UserServiceImpl.java
@@ -63,7 +63,6 @@ public class UserServiceImpl implements UserService, UserDetailsService {
     }
 
     @Override
-    @Transactional
     public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
         User user = userRepository.findUserByLogin(login);
         if (user == null) {
